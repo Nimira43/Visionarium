f2771a68a853737ecf73b38dbfb1a0b260cd5fc2
Nimira43 <n.rabone72@aol.com>
2025-11-22 15:02:23 +0000

    Brought in fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 function App() {
   return (
-    <div>
-      <h1>Project Leonardo</h1>
+    <div className='container mt-5'>
+      <h1 className='text-primary'>Project Leonardo</h1>
+      <button className='btn btn-success'>Click Me</button>
     </div>
   )
 }
