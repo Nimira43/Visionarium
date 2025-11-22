af4bb7261eaa7f330cbf33010d68647d28409608
Nimira43 <n.rabone72@aol.com>
2025-11-22 11:39:51 +0000

    ModeToggle - 1

--- components/ui/them-toggle.tsx
+++ components/ui/them-toggle.tsx
@@ -4,3 +4,13 @@ import { FiSun, FiMoon } from 'react-icons/fi'
 import { useTheme } from 'next-themes'
 import { Button } from './button'
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from './dropdown-menu'
+
+export function ModeToggle() {
+  const {setTheme} = useTheme() 
+
+  return (
+    <DropdownMenu>
+      
+    </DropdownMenu>
+  )
+}
