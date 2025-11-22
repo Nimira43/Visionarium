42c2c01f78b9bbc4d8be3328da9f58e49d94d3d8
Nimira43 <n.rabone72@aol.com>
2025-11-22 11:27:47 +0000

    Bringing ThemeProvider into RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next' 
 import { Poppins } from 'next/font/google'
 import './globals.css' 
+import { ThemeProvider } from '@/lib/theme-provider'
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -18,10 +19,17 @@ export default function RootLayout({
   children: React.ReactNode 
 }>) {
   return (
-    <html lang='en'>
+    <html lang='en' suppressHydrationWarning>
       <body className={poppins.className}>
-        {children}
+        <ThemeProvider
+          attribute = 'class'
+          defaultTheme = 'system'
+          enableSystem
+          disableTransitionOnChange
+        >
+          { children }
+        </ThemeProvider>
       </body>
     </html>
-  ) 
+  )
 }
