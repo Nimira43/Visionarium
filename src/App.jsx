0cb0deb2bf30bf06d36cbf92b49cc447d6994f16
Nimira43 <n.rabone72@aol.com>
2025-11-22 20:40:17 +0000

    Button override styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 function App() {
   return (
     <div className='container mt-5'>
-      <h1 className='text-primary'>Project Leonardo</h1>
-      <button className='btn btn-success'>Click Me</button>
+      <h1 className='text-main'>Project Leonardo</h1>
+      <button className='btn bg-dark text-light'>Click Me</button>
     </div>
   )
 }
