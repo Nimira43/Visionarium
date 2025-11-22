f1b78a72b741b40e4e77507b834ae6e21545bf56
Nimira43 <n.rabone72@aol.com>
2025-11-22 21:18:16 +0000

    App markup - 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 function App() {
   return (
     <div className='app-container'>
-      <div className='header'></div>
-      <div className='tv-show-detail'></div>
-      <div className='recommended-tv-shows'></div>
+      <div className='header'>
+        <div className='row'>
+          <div className='col-4'>
+            <div>Logo</div>
+            <div>Subtitle</div>
+          </div>
+          <div className-md-12 col-4-lg>
+            <input type="text" />
+          </div>
+        </div>
+      </div>
+      <div className='tv-show-detail'>
+        TV Show Details
+      </div>
+      <div className='recommended-tv-shows'>
+        Recommended TV Shows
+      </div>
     </div>
   )
 }
