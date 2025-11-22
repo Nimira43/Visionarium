bad7c8d26c3032b387e334c809d57621e684b139
Nimira43 <n.rabone72@aol.com>
2025-11-22 08:40:36 +0000

    Project created

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { Poppins } from 'next/font/google'
+import "./globals.css";
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
+})
+
+export const metadata: Metadata = {
+  title: "Leonardo",
+  description: 'SaaS application using Next JS, TypeScript, Tailwind, Shadcn, Neon, Prisma, ArcJet and Kinde.',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={poppins.className}>
+        {children}
+      </body>
+    </html>
+  );
+}
+git remote add origin https://github.com/Nimira43/Leonardo.git
