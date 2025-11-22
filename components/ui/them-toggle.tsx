04ccc181fcc53cb703f6b8fd068d62ad9a39bad6
Nimira43 <n.rabone72@aol.com>
2025-11-22 11:55:44 +0000

    Renamed ModeToggle to ThemeToggler. Markup done.

--- components/ui/them-toggle.tsx
+++ components/ui/them-toggle.tsx
@@ -5,7 +5,7 @@ import { useTheme } from 'next-themes'
 import { Button } from './button'
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from './dropdown-menu'
 
-export function ModeToggle() {
+export function ThemeToggle() {
   const {setTheme} = useTheme() 
 
   return (
@@ -19,6 +19,18 @@ export function ModeToggle() {
           <FiMoon className='absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0' />
         </Button>
       </DropdownMenuTrigger>
+      <DropdownMenuContent align='end'>
+        <DropdownMenuItem
+          onClick={() => setTheme('light')}
+        >
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem
+          onClick={() => setTheme('dark')}
+        >
+          Dark
+        </DropdownMenuItem>
+      </DropdownMenuContent>
     </DropdownMenu>
   )
 }
