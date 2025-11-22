2cd6b0bc207af7f91949a84ce7a82eec0be07a79
Nimira43 <n.rabone72@aol.com>
2025-11-22 10:49:13 +0000

    Created ThemeProvider function

--- lib/theme-provider.tsx
+++ lib/theme-provider.tsx
@@ -2,3 +2,13 @@
 
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
 
+export function ThemeProvider({
+  children,
+  ...props
+}: React.ComponentProps<typeof NextThemesProvider>) {
+  return (
+    <NextThemesProvider {...props}>
+      {children}
+    </NextThemesProvider>
+  )
+}
