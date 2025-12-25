89e586dfa128d0d03df2fdb802c0b31ca3a398ef
Nimira43 <n.rabone72@aol.com>
2025-12-25 18:22:30 +0000

    fetch populars - 1

--- src/api/tv-show.js
+++ src/api/tv-show.js
@@ -1,5 +1,9 @@
+import axios from 'axios'
+
 export class TVShowAPI {
   static async fetchPopulars() {
-    
+    const response = await axios.get('urlencoded')
+
+    return response.data.results
   }
 }
