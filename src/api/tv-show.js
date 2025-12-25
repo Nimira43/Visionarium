a353df7176fd2a4091d8ed4d8ae4297412adb0da
Nimira43 <n.rabone72@aol.com>
2025-12-25 19:21:15 +0000

    baseUrl and apiKey

--- src/api/tv-show.js
+++ src/api/tv-show.js
@@ -1,5 +1,8 @@
 import axios from 'axios'
 
+const baseUrl = import.meta.env.VITE_BASE_URL
+const apiKey = import.meta.env.VITE_API_KEY
+
 export class TVShowAPI {
   static async fetchPopulars() {
     const response = await axios.get('urlencoded')
